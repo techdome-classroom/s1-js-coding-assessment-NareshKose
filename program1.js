b8bc63b738f2b376a4598e05e0208648f1bc25fc
Naresh Kose <nareshkose2003@gmail.com>
2024-09-27 12:36:50 +0530

    program1 done

--- program1.js
+++ program1.js
@@ -2,6 +2,52 @@ const getTotalIsles = function (grid) {
 
 
   // write your code here
+    if ( grid.length === 0 || !grid ){
+      
+       return 0;
+
+    }
+
+    const rows = grid.length;
+
+    const columns = grid[0].length;
+
+    const searchRecursion = (i, j) => {
+
+        if (i < 0 || i >= rows || j < 0 || j >= columns || grid[i][j] === 'W') {
+
+            return ;
+        }
+
+        
+        grid[i][j] = 'W';
+
+        
+        searchRecursion(i - 1, j);  
+        searchRecursion(i + 1, j);  
+        searchRecursion(i, j - 1);  
+        searchRecursion(i, j + 1);  
+
+    };
+
+    let numofTotalIslands = 0;
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+
+
+            if (grid[i][j] === 'L') {
+
+              numofTotalIslands++;  
+
+              searchRecursion(i, j);     
+
+            }
+        }
+    }
+
+ return numofTotalIslands;
+
 
 };
 
