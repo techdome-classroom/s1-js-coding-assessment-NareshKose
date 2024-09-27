bbf338778a094849c11a0e914442869c71546939
Naresh Kose <nareshkose2003@gmail.com>
2024-09-27 12:56:52 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -11,7 +11,7 @@ const decodeTheRing = function (s, p) {
   
           if (p[j] === '*') {
               
-              return helperFunctionRec(i, j + 1) || (i < s.length && helperFunctionRec(i + 1, j));
+              return helperFunctionRec(i, j + 1) ;
           }
   
           if (i < s.length && (p[j] === '?' || p[j] === s[i])) {
